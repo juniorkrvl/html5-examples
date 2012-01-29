bbd4e3d701464dce2505fd2e6fb268c0b86fc864
Ray Cohen <raycohen@gmail.com>
2012-01-29 14:55:48 -0500

    remove some dead code

--- emberjs-app/public/javascripts/lists/feed_list.js
+++ emberjs-app/public/javascripts/lists/feed_list.js
@@ -4,13 +4,5 @@ App.feedList = Ember.ArrayProxy.create({
   addFeed: function(feed) {
     var feed = App.Feed.create(feedData);
     this.pushObject(feed);
-  },
-  
-  setAllVideosNotPlaying: function() {
-    this.content.forEach(function(feed) {
-      feed.content.forEach(function(videoItem) {
-        videoItem.set('playing', false);
-      });
-    });
   }  
 });
